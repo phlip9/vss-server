e833d0938ae0fc4482749fcc6cb46b6f46d0a1ef
Philip Kannegaard Hayes <philiphayes9@gmail.com>
2026-09-02 02:18:19 +0000

    pg: fix list_key_versions interpreting keys as patterns
    Using `key LIKE $3` unescaped tells Postgres to interpret the requested
    key prefix as a pattern, which means %, _, and \ are interpreted as
    wildcards and character escapes in the original prefix rather the actual
    intended literal prefix.
    
    This means a `list_key_versions(key="foo_bar", ..)` actually includes
    `foo1bar`, which definitely does not seem intentional.
    
    A better approach is to just use `starts_with(key, $3)`, which the query
    planner understands better and doesn't require us to do any escaping.

--- impls/src/postgres_store.rs
+++ impls/src/postgres_store.rs
@@ -657,7 +657,7 @@ where
 		&self, user_token: String, request: ListKeyVersionsRequest,
 	) -> Result<ListKeyVersionsResponse, VssError> {
 		let store_id = &request.store_id;
-		let key_prefix = &request.key_prefix;
+		let key_prefix = request.key_prefix.as_deref().unwrap_or_default();
 		let page_token = &request.page_token;
 		let page_size = request.page_size.unwrap_or(i32::MAX);
 
@@ -691,16 +691,14 @@ where
 
 		let conn = self.pool.get().await?;
 
-		let key_like = format!("{}%", key_prefix.as_deref().unwrap_or_default());
-
 		let rows = if let Some(ref token) = page_token {
 			let page_sort_order = decode_page_token(token)?;
-			let stmt = "SELECT key, version, sort_order FROM vss_db WHERE user_token = $1 AND store_id = $2 AND sort_order < $3 AND key LIKE $4 AND key != $5 ORDER BY sort_order DESC LIMIT $6";
+			let stmt = "SELECT key, version, sort_order FROM vss_db WHERE user_token = $1 AND store_id = $2 AND sort_order < $3 AND starts_with(key, $4) AND key != $5 ORDER BY sort_order DESC LIMIT $6";
 			let params: Vec<&(dyn tokio_postgres::types::ToSql + Sync)> = vec![
 				&user_token,
 				&store_id,
 				&page_sort_order,
-				&key_like,
+				&key_prefix,
 				&GLOBAL_VERSION_KEY,
 				&fetch_limit,
 			];
@@ -708,9 +706,9 @@ where
 				.await
 				.map_err(|e| Error::new(ErrorKind::Other, format!("Query error: {}", e)))?
 		} else {
-			let stmt = "SELECT key, version, sort_order FROM vss_db WHERE user_token = $1 AND store_id = $2 AND key LIKE $3 AND key != $4 ORDER BY sort_order DESC LIMIT $5";
+			let stmt = "SELECT key, version, sort_order FROM vss_db WHERE user_token = $1 AND store_id = $2 AND starts_with(key, $3) AND key != $4 ORDER BY sort_order DESC LIMIT $5";
 			let params: Vec<&(dyn tokio_postgres::types::ToSql + Sync)> =
-				vec![&user_token, &store_id, &key_like, &GLOBAL_VERSION_KEY, &fetch_limit];
+				vec![&user_token, &store_id, &key_prefix, &GLOBAL_VERSION_KEY, &fetch_limit];
 			conn.query(stmt, &params)
 				.await
 				.map_err(|e| Error::new(ErrorKind::Other, format!("Query error: {}", e)))?
